6b764ff7958722e51f13d21a68a6fef9d336a378
Alex Noble and David Tumpowsky <alex@>
2018-03-29 09:50:37 -0700

    add functionality for numbers 1 thru 99

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
 var romanNumeral;
+var tens;
+var romanString;
+
+function ones(remainder){
+  if (remainder===1){
+    romanString=romanString+"I";
+  } else if (remainder===2){
+    romanString=romanString+"II";
+  } else if(remainder===3){
+    romanString=romanString+"III";
+  } else if(remainder===4){
+    romanString=romanString+"IV";
+  } else if (remainder===5) {
+    romanString=romanString+"V";
+  } else if (remainder===6){
+    romanString=romanString+"VI";
+  } else if (remainder===7){
+    romanString=romanString+"VII";
+  } else if (remainder===8){
+    romanString=romanString+"VIII";
+  } else if (remainder===9){
+    romanString=romanString+"IX";
+  }
+}
 
 
 
@@ -7,11 +31,45 @@ $(document).ready(function() {
     event.preventDefault();
 
     var number = parseInt($("input#decimal").val());
-    if (number===1){
-      romanNumeral="I";
+    romanString="";
+    tens=parseInt(number/10);
+    console.log(tens);
+    if (tens===0){}
+    else if (tens===4){
+      romanString=romanString+"XL";
+    } else if(tens==9){
+      romanString=romanString+"XC";
     }
+    else if (tens>4){
+      tens=tens-5;
+      romanString=romanString+"L";
+        for (i=1; i<=tens; i++){
+          romanString=romanString+"X";
+        }
+    }  else{
+      for (i=1; i<=tens; i++){
+        romanString=romanString+"X";
+      }
+    }
+    remainder=number%10;
+    ones(remainder);
 
 
-    $("#result").text(romanNumeral);
+
+    // } else if (number===10) {
+    //   romanNumeral="X";
+    // }
+    //
+    // else if (number===20){
+    //   romanNumeral="XX"
+    // } else if (number===30) {
+    //   romanNumeral="XX"
+    // }
+
+
+
+
+    $("#result").text(romanString);
+  //console.log(romanNumeral);
   });
 });
