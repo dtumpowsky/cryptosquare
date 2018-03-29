698a4ae25ad5e592e75d45bcba08d79880212dd4
Alex Noble and David Tumpowsky <david@>
2018-03-29 15:21:11 -0700

    display a functioning cryptosquare application

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,10 @@ var rows;
 var columns;
 var intSquare;
 var finalLetters=[];
+var finalString="";
 var i=0;
 var j=0;
+var counter=0;
 
 $(document).ready(function() {
   $("form#converter").submit(function(event) {
@@ -41,19 +43,46 @@ $(document).ready(function() {
     console.log ("The square needs " +rows+ " rows and "+columns+" columns.");
 
     letters.reverse();
-
+    while (letters.length!=0){
     for(i=0; i<rows;i++){
       finalLetters[i]=new Array(j); //makes an array of arrays
       for(j=0; j<columns; j++){
-        finalLetters[i][j]=letters.pop();
+        if (letters.length===0){
+          finalLetters[i][j]="";
+        }
+        else{
+          finalLetters[i][j]=letters.pop();
+        }
+
       }
     }
+  }
+
+
+    for(x=0; x<columns; x++)
+    {
+      for(y=0; y<rows; y++)
+      {
+        if (counter%5==0){
+          finalString=finalString+" "+ finalLetters[y][x];
+        }
+        else{
+          finalString=finalString + finalLetters[y][x];
+        }
+        counter++;
+      }
+    }
+
+
+
+
     console.log(finalLetters);
+    console.log(finalString);
 
 
 
 
-    $("#result").text(sentence);
+    $("#result").text(finalString);
 
   });
 });
