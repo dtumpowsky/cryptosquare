f96af188ac4eee349e379ae281b4216e6339a005
Alex Noble and David Tumpowsky <david@>
2018-03-29 14:13:22 -0700

    add functionality for determining length of user input, remove spaces, and stores in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
+var num;
+var squaredNum;
+var letters=[];
+
+
 
 $(document).ready(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
     var sentence = $("input#decimal").val();
-    console.log(sentence.length);
+    sentence=sentence.replace(/\s/g, ''); //removes spaces
 
+    letters=sentence.split("")
+
+    console.log("size of letters array" +letters.length);
+
+    num=letters.length;
+    squaredNum=Math.sqrt(num);
+
+    console.log(squaredNum);
 
 
     $("#result").text(sentence);
