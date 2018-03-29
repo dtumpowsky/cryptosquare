fd723ea7d8c9732cef20eea78b63464624e1b5d6
Alex Noble and David Tumpowsky <alex@>
2018-03-29 09:01:35 -0700

    add first spec functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
+var romanNumeral;
+
+
+
 $(document).ready(function() {
   $("form#converter").submit(function(event) {
     event.preventDefault();
 
-    var number = $("input#decimal").val();
+    var number = parseInt($("input#decimal").val());
+    if (number===1){
+      romanNumeral="I";
+    }
 
+
+    $("#result").text(romanNumeral);
   });
 });
